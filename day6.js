d5848851938a658a90f9daa9f73f44ff1a330369
Kartikey Rai <Kartikeyarai7@gmail.com>
2023-07-30 23:53:40 +0400

    PalindromeCheck

---
+++ day6.js
@@ -0,0 +1,16 @@
+var isPalindrome = function (s) {
+  let b = s.toLowerCase().split('');
+  let c = [];
+  b.forEach(item => {
+    let range = item.charCodeAt();
+    if ((range > 96 && range < 123) || (range > 47 && range < 58)) {
+      c.push(item);
+    }
+  });
+  if (c.join('') === c.reverse().join('')) {
+    return true;
+  }
+  return false;
+};
+
+// Concept of ASCII to filter out non alpha-numeric char
