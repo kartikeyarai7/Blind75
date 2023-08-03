2202482c12635583a334f698c6ee1f18e56ae867
Kartikey Rai <61941790+kartikeyarai7@users.noreply.github.com>
2023-08-03 16:49:55 +0400

    Add files via upload
    Bit Reversal and Hamming Weight Problem - Count 1
    JS interpretation of bits

---
+++ day10.js
@@ -0,0 +1,24 @@
+var reverseBits = function(n) {
+    let result = 0;
+    for (let i = 0; i < 32; i++) {
+        result = (result << 1) | (n & 1);
+        n >>= 1;
+    }
+    return result >>> 0; // Ensure 32-bit unsigned result
+};
+
+// Reverse unsigned integer
+// Key Idea : Bitwise Operators
+// to be treated as binary, it needs to 0b0101... in JS, by default is octal
+
+// Treat like string then 
+var reverseBits = function(n) {
+    let n1 = n.toString(2).split("")
+  let n2 = n.toString(2).split("").reverse()
+    let num1 = 0;
+    for(let i=0;i<n2.length;i++){
+        num1 = num1 + ((Math.pow(2,n2.length-1-i))*n2[i])
+    }
+ 
+    return num1; 
+};
